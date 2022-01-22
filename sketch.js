370144a796961806f125ea9c7a3e1869f8394a74
jannaveve <53411676+jannaveve@users.noreply.github.com>
2022-01-21 20:44:43 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,104 @@
+// The RiTa lib is included in index.html
+// The text files are files in the project
+var bg = [];
+let avgRed = 0;
+let avgGreen = 0;
+let avgBlue = 0;
+let cg;
+let lines, markov, txt1, txt2;
+let x = 400,
+    y = 100;
+let myFont;
+const model = new rw.HostedModel({
+  url:
+    "https://continue-samplerexperiment-ee1dc17a.hosted-models.runwayml.cloud/v1/",
+  token: "4FkmPeeLT4cKGY4wYXMlnw==",
+});
+
+function preload() {
+  myFont = loadFont("data1/Goodbyecrewelworldnf-mLVeP.otf");
+  txt1 = loadStrings("data1/Mitch.txt");
+  txt2 = loadStrings("data1/condolences.txt");
+  for (var i = 0; i < 25; i++) {
+    bg[i] = loadImage("img/img" + i + ".png");
+  }
+}
+
+function setup() {
+  let c = createCanvas(576, 384);
+  cg = createGraphics(576, 384);
+
+  textFont(myFont, 24);
+  textLeading(36);
+  textAlign(LEFT);
+
+  lines = ["Click for Condolence"];
+
+
+  markov = RiTa.markov(3);
+
+  // load text into the model
+  markov.addText(txt1.join(" "));
+  markov.addText(txt2.join(" "));
+
+  drawText();
+}
+
+function drawText() {
+  background(255);
+  var r = floor(random(bg.length));
+  
+  noStroke();
+  tint(255, 255);
+  image(bg[r], 0, 0, 384, 384);
+    // Load the pixels
+  bg[r].loadPixels();
+
+  // Loop through the pixels X and Y
+  for (let y = 0; y < bg[r].height; y++) {
+    for (let x = 0; x < bg[r].width; x++) {
+
+      // Calculate the pixel index
+      const index = (y * bg[r].width + x) * 4;
+
+      // Sum the red, green, and blue values
+      avgRed += bg[r].pixels[index + 0];
+      avgGreen += bg[r].pixels[index + 1];
+      avgBlue += bg[r].pixels[index + 2];
+
+    }
+  }
+
+  // We're finished working with pixels so update them
+  bg[r].updatePixels();
+
+  // Get the total number of pixels
+  // Divide by 4 because the total number of pixels = pixels * numColorChannels 
+  const numPixels = bg[r].pixels.length / 4;
+
+  // divide the totals by the number of pixels to find the average.
+  avgRed /= numPixels;
+  avgGreen /= numPixels;
+  avgBlue /= numPixels;
+  
+
+    // Set the fill color to the average color of the pixels
+  fill(avgRed, avgGreen, avgBlue,150);
+  rectMode(CENTER);
+  rect(width-95, height / 2, 160, 340);
+  //image(cg,0,0);
+  fill(60,20,20);
+  textSize(16);
+  text(lines.join(" "), width-90, height/2, 150, 340);
+  console.log(r);
+
+}
+
+function mouseClicked() {
+  lines = markov.generate(2);
+  textSize(16);
+  x = 350;
+  y = 300;
+  drawText();
+  //saveCanvas( 'myCanvas', 'png')
+}
